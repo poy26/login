daf33ca07ec2b5c2d0f2703c2243f8117f1934da
Tn.Yop_Engineering <poycorpsdev@gmail.com>
2019-09-04 14:07:13 +0700

    Add files via upload

---
+++ login.sh
@@ -0,0 +1,27 @@
+#codedBy T_CSN~Tn.Yop_Tech
+#Tangsel Cyber Security Noobs
+
+echo "\033[31;1m"
+echo "
+	_|          _|_|      _|_|_|  _|_|_|  _|      _|
+	_|        _|    _|  _|          _|    _|_|    _|
+	_|        _|    _|  _|  _|_|    _|    _|  _|  _|
+	_|        _|    _|  _|    _|    _|    _|    _|_|
+	_|_|_|_|    _|_|      _|_|_|  _|_|_|  _|      _| "
+echo "\033[31;1m"
+echo "\033[36;1m	[$]\033[36;1m==================""♤Tn.Yop♤""==================\033[36;1m[$]"
+echo
+echo "\033[36;1m	This Devices Locked By T_CSN~Tn.Yop_Tech<InDo"
+echo "\033[36;1m	Please Enter Your Username & Password"
+echo
+read -p "	USERNAME : " poy
+if [	 $poy = poy ]
+then
+sh password.sh
+else
+echo
+echo "	wrong username!"
+sleep 1
+clear
+sh login.sh
+fi
